1e2c86ecddde4e23e64b11fced60cb5b46072717
Ichii <45918948+IchiiDev@users.noreply.github.com>
2021-01-28 22:56:53 +0100

    Changed configuration

--- assets/config.js
+++ assets/config.js
@@ -1,24 +1,27 @@
 const config = {
     
-    USERNAME: "User#0000",
+    USERNAME: "Ichii#5999",
     PICTURE: "assets/img/profile_picture.png",
 
-    DESCRIPTION: "Change this bio in the <b>assets/config.js</b> file. <i>It may include HTML syntax</i>",
+    DESCRIPTION: "Proud developer making dumb projects on GitHub, sucks at Front-End & currently working remotely for <a href=\"https://github.com/WyvernTeam\" target=\"_blank\">@WyvernTeam</a> & <a href=\"https://discord.fr\" target=\"_blank\">@discordfr</a>",
 
     BADGES: [
+        { id: "HypeSquad Events Member", image: "https://discord.com/assets/6c73f47daf179ffade99f501bfc5101b.svg" },
         { id: "HypeSquad Bravery", image: "https://discord.com/assets/64ae1208b6aefc0a0c3681e6be36f0ff.svg" },
+        { id: "Early Verified Bot Developer", image: "https://discord.com/assets/45cd06af582dcd3c6b79370b4e3630de.svg" },
         { id: "Nitro Sub", image: "https://discord.com/assets/386884eecd36164487505ddfbac35a9d.svg" },
+        { id: "Nitro Boosting since 1 year", image: "https://discord.com/assets/7a5f78de816fcecbbd1d5d6e635cc7dd.svg" },
     ],
 
     PROJECTS: [
-        { id: "project-1", image: "assets/img/placeholder1.png", link: "https://discord.com", name: "Super cool project 1" },
-        { id: "project-2", image: "assets/img/placeholder2.png", link: "https://discord.com", name: "Super cool project 2" },
-        { id: "project-3", image: "assets/img/placeholder3.png", link: "https://discord.com", name: "Super cool project 3" },
+        { id: "slash-fr", image: "assets/img/slashfr.webp", link: "https://discord.gg/fr", name: "Slash FR" },
+        { id: "discord-fr", image: "assets/img/discordfr.svg", link: "https://discord.fr", name: "Discord FR" },
+        { id: "wyvern", image: "assets/img/wyvern.png", link: "https://wyvern.team", name: "Team Wyvern" },
     ],
 
     SOCIALS: [
-        { class_name: "fab fa-github", link: "https://github.com/discord" },
-        { class_name: "fab fa-twitter", link: "https://twitter.com/discord" },
+        { class_name: "fab fa-github", link: "https://github.com/IchiiDev" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/IchiiDev" },
     ],
 
 }
