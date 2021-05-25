3051a931bf047b54458dd8bfe8a22ef212cfd3fd
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-05-25 19:22:30 +0200

    Edit config

--- assets/config.js
+++ assets/config.js
@@ -10,18 +10,18 @@ const config = {
         { id: "HypeSquad Bravery", image: "https://discord.com/assets/64ae1208b6aefc0a0c3681e6be36f0ff.svg" },
         { id: "Early Verified Bot Developer", image: "https://discord.com/assets/45cd06af582dcd3c6b79370b4e3630de.svg" },
         { id: "Nitro Sub", image: "https://discord.com/assets/386884eecd36164487505ddfbac35a9d.svg" },
-        { id: "Nitro Boosting since 1 year", image: "https://discord.com/assets/7a5f78de816fcecbbd1d5d6e635cc7dd.svg" },
+        { id: "Nitro Boosting since 15 month", image: "https://discord.com/assets/5a24b20b84fb3eafc138916729386e76.svg" },
     ],
 
     PROJECTS: [
         { id: "slash-fr", image: "assets/img/slashfr.webp", link: "https://discord.gg/fr", name: "Slash FR" },
-        { id: "discord-fr", image: "assets/img/discordfr.svg", link: "https://discord.fr", name: "Discord FR" },
+        { id: "discord-fr", image: "assets/img/discordfr.svg", link: "https://discord.fr", name: "Discord.FR" },
         { id: "wyvern", image: "assets/img/wyvern.png", link: "https://wyvern.team", name: "Team Wyvern" },
     ],
 
     SOCIALS: [
-        { class_name: "fab fa-github", link: "https://github.com/IchiiDev" },
-        { class_name: "fab fa-twitter", link: "https://twitter.com/IchiiDev" },
+        { class_name: "fab fa-github", link: "https://github.com/ichiidev" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/ichiidev" },
     ],
 
 }
